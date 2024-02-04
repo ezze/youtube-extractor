c618e95a78c5a325a54343c3861e86a26c7c0057
Dmitriy Pushkov <ezze@ezze.org>
2024-02-05 01:21:22 +0300

    Reading Youtube links from files is supported

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,10 @@
 import { app } from 'electron';
+import path from 'path';
+import readline from 'readline';
 
 import { Command } from 'commander';
+import fs from 'fs-extra';
+import { getVideoID } from 'ytdl-core';
 
 import { processMedia } from './youtube';
 
@@ -8,28 +12,80 @@ type ProgramOptions = {
   output?: string;
 };
 
-export function runCliApp(): void {
-  app.on('ready', () => {
-    const program = new Command();
+async function readSourceFile(sourceFilePath: string): Promise<Array<string>> {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(sourceFilePath),
+    crlfDelay: Infinity
+  });
 
-    program
-      .argument('<videos...>')
-      .option('-o, --output <output>', 'output directory path')
-      .action(async (items: Array<string>, options: ProgramOptions) => {
-        const { output = process.cwd() } = options;
-        console.log(items);
-        for (let i = 0; i < items.length; i += 1) {
-          const item = items[i];
+  const videoIds: Array<string> = [];
+  for await (const line of rl) {
+    try {
+      videoIds.push(getVideoID(line));
+    } catch (e) {
+      console.error(`"${line}" is not a valid Youtube video URL or ID`);
+    }
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(`Line from file: ${line}`);
+  }
+  return videoIds;
+}
+
+function onReady(): void {
+  const program = new Command();
+
+  program
+    .argument('<videos...>')
+    .option('-o, --output <output>', 'output directory path')
+    .action(async (items: Array<string>, options: ProgramOptions) => {
+      console.log('Analyzing input...');
+      const videoIds: Array<string> = [];
+      for (let i = 0; i < items.length; i += 1) {
+        const item = items[i];
+        if (fs.existsSync(item)) {
           try {
-            console.log(`Processing "${item}"...`);
-            await processMedia(item, output);
+            videoIds.push(...(await readSourceFile(item)));
           } catch (e) {
-            console.error(e);
+            console.error(`Unable to read source file "${item}".`);
+          }
+        } else {
+          try {
+            videoIds.push(getVideoID(item));
+          } catch (e) {
+            console.error(`"${item}" is not a valid Youtube video URL or ID`);
           }
         }
-        app.quit();
+      }
+
+      if (videoIds.length === 0) {
+        console.warn('There are no valid Youtube video sources detected');
+        app.exit(1);
+      }
+
+      console.log('The following Youtube video IDs are detected:');
+      videoIds.forEach((videoId) => {
+        console.log(`- ${videoId}`);
       });
 
-    program.parse();
-  });
+      const { output = process.cwd() } = options;
+      const outputPath = path.isAbsolute(output) ? output : path.resolve(process.cwd(), output);
+      console.log(`The following output directory is detected: ${outputPath}`);
+
+      for (let i = 0; i < videoIds.length; i += 1) {
+        const youtubeId = videoIds[i];
+        try {
+          console.log(`Processing "${youtubeId}"...`);
+          await processMedia(youtubeId, outputPath);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      app.quit();
+    });
+
+  program.parse();
+}
+
+export function runCliApp(): void {
+  app.on('ready', onReady);
 }
