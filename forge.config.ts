0674187cc1890d19bb0a3c6c07930375017ee950
Dmitriy Pushkov <ezze@ezze.org>
2024-02-04 15:44:41 +0300

    Bundle size is optimized

--- forge.config.ts
+++ forge.config.ts
@@ -1,22 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import path from 'path';
+
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import { createLogger, format, transports } from 'winston';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 import type { ForgeConfig } from '@electron-forge/shared-types';
 
+const logger = createLogger({
+  level: 'info',
+  format: format.simple(),
+  transports: [new transports.File({ filename: path.resolve(__dirname, 'forge.config.log'), options: { flags: 'w' } })]
+});
+
 const config: ForgeConfig = {
   packagerConfig: {
     asar: {
       unpackDir: 'node_modules/ffmpeg-static'
     },
-    ignore: []
+    ignore: [/^\/(?!(\.webpack|node_modules$|node_modules\/ffmpeg-static|package\.json))/]
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
